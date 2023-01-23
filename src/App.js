90ee98ed5ef62b6515972a05ea835275b29ba43b
Alef Lewitt <66555084+Alef5750@users.noreply.github.com>
2023-01-23 13:51:51 +0200

    rendering list of tasks in Tasks component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30 PM",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Meeting at school",
+      day: "Feb 6th at 1:30 PM",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30 PM",
+      reminder: false,
+    },
+  ]);
   return (
     <div className="container">
       <Header name="Alfalfa" />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
